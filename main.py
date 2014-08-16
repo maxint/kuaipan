5ad3b1f67174ecf7d11234ffda8b1df3f0c1756d
maxint <lnychina@gmail.com>
2014-08-16 22:22:08 +0800

    Fix bug of file stat updating

--- main.py
+++ main.py
@@ -40,92 +40,33 @@ def get_time(time_str):
     return time.mktime(time.strptime(time_str, "%Y-%m-%d %H:%M:%S"))
 
 
-def create_st(meta):
-    if meta.get('path') == '/':
-        now = time.time()
+def create_stat(isdir, size=0, ctime=None, mtime=None):
+    now = time.time()
+    if ctime is None:
+        ctime = now
+    if mtime is None:
+        mtime = now
+    if isdir:
         return dict(st_mode=(stat.S_IFDIR | 0644), st_nlink=2,
-                    st_ctime=now, st_mtime=now, st_atime=now)
-    if meta['type'] == 'folder':
-        attrs = dict(st_mode=(stat.S_IFDIR | 0644), st_nlink=2)
+                    st_ctime=ctime, st_mtime=mtime, st_atime=mtime)
     else:
-        attrs = dict(st_mode=(stat.S_IFREG | 0644), st_nlink=1,
-                     st_size=meta['size'])
-    attrs['st_ctime'] = get_time(meta['create_time'])
-    attrs['st_mtime'] = attrs['st_atime'] = get_time(meta['modify_time'])
-    return attrs
+        return dict(st_mode=(stat.S_IFREG | 0644), st_nlink=1, st_size=0,
+                    st_ctime=ctime, st_mtime=mtime, st_atime=mtime)
 
 
-class TreeCache():
-    def __init__(self, kp):
-        self.kp = kp
-        self.tree = dict()
-        self.build('/', self.tree)
-
-    def get_metadata(self, path):
-        meta = self.kp.metadata(path)
-        if meta and 'path' in meta:
-            return meta
-
-    def build(self, path, node):
-        meta = self.get_metadata(path)
-        if meta is None:
-            return
-
-        node['attrs'] = create_st(meta)
-        if meta.get('path') == '/' or meta['type'] == 'folder':
-            node['type'] = 'folder'
-            files = meta.get('files', [])
-            if 'dirs' not in node:
-                node['dirs'] = [x['name'] for x in files] + ['.', '..']
-            if 'files' not in node:
-                fnodes = dict()
-                for x in files:
-                    fnodes[x['name']] = dict(attrs=create_st(x),
-                                             type=x['type'])
-                node['files'] = fnodes
-        else:
-            node['type'] = 'file'
-
-        return node
-
-    def getnode(self, path, dirs=False):
-        if path == '/':
-            return self.tree
-
-        names = path.strip('/').split('/')
-        node = self.tree
-        cur_dir = '/'
-        for name in names:
-            if node.get('type') != 'folder':
-                return
-
-            if 'files' not in node:
-                if self.build(cur_dir, node) is None:
-                    return
-            node = node.get('files').get(name)
-            if node is None:
-                return
-            cur_dir += name + '/'
-
-        #if dirs:
-            #print path; import ipdb; ipdb.set_trace()
-        if node.get('type') == 'folder' and 'dirs' not in node and dirs:
-            self.build(path, node)
-        return node
-
-    def getattr(self, path):
-        node = self.getnode(path, False)
-        if node:
-            return node['attrs']
-
-    def readdir(self, path):
-        node = self.getnode(path, True)
-        return node['dirs']
+def create_stat2(meta):
+    if meta.get('path') == '/':
+        return create_stat(True)
+    else:
+        return create_stat(meta['type'] == 'folder',
+                           meta.get('size', 0),
+                           get_time(meta['create_time']),
+                           get_time(meta['modify_time']))
 
 
 class ContentCache():
-    def __init__(self, r, tsize=0):
-        self.raw = r.raw
+    def __init__(self, r=None, tsize=0):
+        self.raw = r
         self.data = bytes()
         self.tsize = tsize
         self.modified = False
@@ -160,8 +101,102 @@ class ContentCache():
 class KuaipanFuse(fuse.LoggingMixIn, fuse.Operations):
     def __init__(self, kp):
         self.kp = kp
-        self.tree = TreeCache(kp)
-        self.read_cache = dict()
+        self.data_caches = dict()
+        self.tree = dict()
+        self.build('/')
+
+    def build(self, path, node=None):
+        """
+        {
+            type: 'file' or 'folder'
+            attrs: {
+                st_mode:
+                st_nlink:
+                st_ctime:
+                st_atime:
+                st_mtime:
+            }
+            [dirs]: ['.', '..', ...]
+            [files]: {
+                'path': {
+                    type:
+                    attrs:
+                    [dirs]:
+                    [files]:
+                }
+            }
+        }
+        """
+        if node is None:
+            node = self.get_node(path)
+        meta = self.kp.metadata(path)
+        if meta is None:
+            return
+
+        node['attrs'] = create_stat2(meta)
+        if meta.get('path') == '/' or meta['type'] == 'folder':
+            node['type'] = 'folder'
+            files = meta.get('files', [])
+            if 'dirs' not in node:
+                node['dirs'] = [x['name'] for x in files] + ['.', '..']
+            if 'files' not in node:
+                fnodes = dict()
+                for x in files:
+                    fnodes[x['name']] = dict(type=x['type'],
+                                             attrs=create_stat2(x),
+                                             )
+                node['files'] = fnodes
+        else:
+            node['type'] = 'file'
+
+        return node
+
+    def get_node(self, path, dirs=False):
+        if path == '/':
+            return self.tree
+
+        names = path.strip('/').split('/')
+        node = self.tree
+        cur_dir = '/'
+        for name in names:
+            if node.get('type') != 'folder':
+                return
+
+            if 'files' not in node:
+                if self.build(cur_dir, node) is None:
+                    return
+            node = node.get('files').get(name)
+            if node is None:
+                return
+            cur_dir += name + '/'
+
+        if node.get('type') == 'folder' and 'dirs' not in node and dirs:
+            self.build(path, node)
+        return node
+
+    def create_node(self, path, isdir):
+        node = dict(type='folder' if isdir else 'file',
+                    attrs=create_stat(isdir))
+        return self.insert_node(path, node)
+
+    def pop_node(self, path):
+        adir, aname = os.path.split(path)
+        pnode = self.get_node(adir)
+        if pnode:
+            pnode['dirs'].remove(aname)
+            pnode['attrs']['st_mtime'] = time.time()
+            return pnode['files'].pop(aname, None)
+
+    def insert_node(self, path, node):
+        adir, aname = os.path.split(path)
+        pnode = self.get_node(adir)
+        if pnode:
+            pnode['files'][aname] = node
+            pnode['dirs'].append(aname)
+            pnode['attrs']['st_mtime'] = time.time()
+        return node
+
+    #----------------------------------------------------
 
     def access(self, path, amode):
         log.debug("access: %s, amode=%s", path, str(amode))
@@ -169,39 +204,45 @@ class KuaipanFuse(fuse.LoggingMixIn, fuse.Operations):
 
     def getattr(self, path, fh=None):
         log.debug("getattr: %s, fh=%s", path, str(fh))
-        attrs = self.tree.getattr(path)
-        if not attrs:
+        node = self.get_node(path, False)
+        if not node:
             log.debug("getattr: %s not exists!", path)
             raise fuse.FuseOSError(errno.ENOENT)
-        return attrs
+        return node['attrs']
 
     def readdir(self, path, fh):
         log.debug("readdir: %s, fh=%s", path, str(fh))
-        return self.tree.readdir(path)
+        return self.get_node(path, True)['dirs']
 
     def rename(self, old, new):
         log.debug("rename %s to %s", old, new)
         self.kp.move(old, new)
-        #self.all_dir = self.walk_recursion("/")
+        # update
+        node = self.pop_node(old)
+        if node:
+            self.insert_node(new, node)
 
     def mkdir(self, path, mode=0644):
         log.debug("mkdir: %s, mode=%d", path, mode)
         self.kp.mkdir(path)
-        #self.all_dir = self.walk_recursion("/")
+        self.create_node(path, True)
 
     def rmdir(self, path):
         log.debug("rmdir: %s", path)
         self.kp.delete(path)
-        #self.all_dir = self.walk_recursion("/")
+        self.pop_node(path)
 
-    unlink = rmdir
+    def unlink(self, path):
+        log.debug("unlink: %s", path)
+        self.rmdir(path)
+        self.data_caches.pop(path)
 
     def open(self, path, flags):
         log.debug("open: %s, flags=%d", path, flags)
-        if not path in self.read_cache:
+        if not path in self.data_caches:
             st_size = self.tree.getattr(path)['st_size']
-            it = ContentCache(self.kp.download(path), st_size)
-            self.read_cache[path] = it
+            it = ContentCache(self.kp.download(path).raw, st_size)
+            self.data_caches[path] = it
         return 0
 
     def release(self, path, fh):
@@ -211,38 +252,42 @@ class KuaipanFuse(fuse.LoggingMixIn, fuse.Operations):
     def read(self, path, size, offset, fh):
         log.debug("read: %s, size=%d, offset=%d, fh=%s",
                   path, size, offset, str(fh))
-        it = self.read_cache[path]
+        it = self.data_caches[path]
         return it.read(size, offset)
 
     def truncate(self, path, length, fh=None):
         log.debug("truncate: %s, length=%d, fh=%s",
                   path, length, str(fh))
-        it = self.read_cache[path]
+        it = self.data_caches[path]
         it.truncate(length)
-        # TBD: update
+        node = self.get_node(path)
+        node['attrs']['st_size'] = length
 
     def write(self, path, data, offset, fh):
         log.debug("write: %s, data=%s, offset=%d, fh=%s",
                   path, cap_string(data, 10),  offset, str(fh))
-        it = self.read_cache[path]
+        it = self.data_caches[path]
         it.write(data, offset)
-        # TBD: update
+        node = self.get_node(path)
+        node['attrs']['st_size'] = offset + len(data)
         return len(data)
 
     def flush(self, path, fh):
         log.debug("flush: %s, fh=%s", path, str(fh))
-        it = self.read_cache[path]
+        it = self.data_caches[path]
         it.flush(path, self.kp)
-        # TBD: update
+        node = self.get_node(path)
+        node['attrs']['st_mtime'] = time.time()
         return 0
 
     def create(self, path, mode=0644, fi=None):
-        log.debug("create: %s, mode: %d, fi=%d", path, mode, fi)
+        log.debug("create: %s, mode: %d, fi=%s", path, mode, str(fi))
+        self.data_caches[path] = ContentCache()
+        self.create_node(path, False)
         name = os.path.basename(path)
         if name.startswith('.~') or name.startswith('~'):
             return
         self.kp.upload(path, '', True)
-        # TBD: update
         return 0
 
 
